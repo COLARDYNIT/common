9096abe41d848611a5f573f79936bbcf7964eb2e
Frederik Colardyn <frederikcolardyn@hwbot.org>
2014-04-01 00:51:46 +0200

    dto

---
+++ hwbot-common/src/main/java/org/hwbot/api/bench/dto/NotificationsDTO.java
@@ -0,0 +1,25 @@
+package org.hwbot.api.bench.dto;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
+
+@JsonSerialize(include = Inclusion.NON_NULL)
+public class NotificationsDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private List<NotificationDTO> list = new ArrayList<NotificationDTO>();
+
+    public List<NotificationDTO> getList() {
+        return list;
+    }
+
+    public void setList(List<NotificationDTO> list) {
+        this.list = list;
+    }
+
+}
