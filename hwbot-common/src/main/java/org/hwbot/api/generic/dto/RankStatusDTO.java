5643c818492e1a1188aca90786d8bd7d639659bf
Frederik Colardyn <frederikcolardyn@hwbot.org>
2014-04-02 23:36:30 +0200

    rank status

---
+++ hwbot-common/src/main/java/org/hwbot/api/generic/dto/RankStatusDTO.java
@@ -0,0 +1,56 @@
+package org.hwbot.api.generic.dto;
+
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
+
+@JsonSerialize(include = Inclusion.NON_NULL)
+public class RankStatusDTO {
+
+    private SubmissionDTO better;
+    private SubmissionDTO worse;
+    private SubmissionDTO me;
+
+    private int rank;
+    private int total;
+
+    public SubmissionDTO getBetter() {
+        return better;
+    }
+
+    public void setBetter(SubmissionDTO better) {
+        this.better = better;
+    }
+
+    public SubmissionDTO getWorse() {
+        return worse;
+    }
+
+    public void setWorse(SubmissionDTO worse) {
+        this.worse = worse;
+    }
+
+    public SubmissionDTO getMe() {
+        return me;
+    }
+
+    public void setMe(SubmissionDTO me) {
+        this.me = me;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+}
