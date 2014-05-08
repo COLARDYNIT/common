9b26aed62bbbc89e79eabac246fcfb70fdb01499
Frederik Colardyn <frederikcolardyn@hwbot.org>
2014-05-08 09:33:13 +0200

    pictures for esports

--- hwbot-common/src/main/java/org/hwbot/api/esports/ArticleDTO.java
+++ hwbot-common/src/main/java/org/hwbot/api/esports/ArticleDTO.java
@@ -19,6 +19,8 @@ import org.hwbot.util.StringUtil;
 public class ArticleDTO {
 
     private String title;
+    // optional, for index page banner
+    private String subtitle;
     private Integer id;
     private String url;
     private long time;
@@ -26,161 +28,135 @@ public class ArticleDTO {
     // optional
     private String author;
     private String content;
+    // for news overview or index page banner
     private String excerpt;
-    private String tinyPicture;
-    private String smallPicture;
+    // 72 x 60, for small list
+    private String thumbPicture;
+    // 760 x 570, for news overview
+    private String mediumPicture;
+    // 1000 x 450 for news detail
     private String largePicture;
+    // 2200 x 800, for index page banner, optional
+    private String bannerPicture;
+
     private int amountOfComments;
     private List<String> tags;
     private List<CommentDTO> comments;
 
-    public ArticleDTO() {
-    }
-
-    /**
-     * Constructor for overview
-     */
-    public ArticleDTO(String title, Integer id, String url, long time, String excerpt, String smallPicture, int amountOfComments, List<String> tags) {
-        super();
-        this.title = title;
-        this.id = id;
-        this.url = url;
-        this.time = time;
-        this.excerpt = excerpt;
-        this.smallPicture = smallPicture;
-        this.amountOfComments = amountOfComments;
-        this.tags = tags;
-    }
-
-    /**
-     * Constructor for detail view
-     */
-    public ArticleDTO(String title, Integer id, String url, long time, String author, String content, String largePicture, int amountOfComments,
-            List<String> tags, List<CommentDTO> comments) {
-        super();
-        this.title = title;
-        this.id = id;
-        this.url = url;
-        this.time = time;
-        this.author = author;
-        this.content = content;
-        this.largePicture = largePicture;
-        this.amountOfComments = amountOfComments;
-        this.tags = tags;
-        this.comments = comments;
-    }
-
-    /**
-     * Constructor for list
-     */
-    public ArticleDTO(String title, Integer id, String url, long time, String tinyPicture, int amountOfComments) {
-        super();
-        this.title = title;
-        this.id = id;
-        this.url = url;
-        this.time = time;
-        this.tinyPicture = tinyPicture;
-        this.amountOfComments = amountOfComments;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
     public String getTitle() {
         return title;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public long getTime() {
-        return time;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public String getExcerpt() {
-        return excerpt;
-    }
-
-    public String getTinyPicture() {
-        return tinyPicture;
-    }
-
-    public String getSmallPicture() {
-        return smallPicture;
-    }
-
-    public String getLargePicture() {
-        return largePicture;
-    }
-
-    public int getAmountOfComments() {
-        return amountOfComments;
-    }
-
-    public List<String> getTags() {
-        return tags;
-    }
-
-    public List<CommentDTO> getComments() {
-        return comments;
     }
 
     public void setTitle(String title) {
         this.title = title;
     }
 
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
     }
 
     public void setUrl(String url) {
         this.url = url;
     }
 
+    public long getTime() {
+        return time;
+    }
+
     public void setTime(long time) {
         this.time = time;
+    }
+
+    public String getAuthor() {
+        return author;
     }
 
     public void setAuthor(String author) {
         this.author = author;
     }
 
+    public String getContent() {
+        return content;
+    }
+
     public void setContent(String content) {
         this.content = content;
+    }
+
+    public String getExcerpt() {
+        return excerpt;
     }
 
     public void setExcerpt(String excerpt) {
         this.excerpt = excerpt;
     }
 
-    public void setTinyPicture(String tinyPicture) {
-        this.tinyPicture = tinyPicture;
+    public String getThumbPicture() {
+        return thumbPicture;
     }
 
-    public void setSmallPicture(String smallPicture) {
-        this.smallPicture = smallPicture;
+    public void setThumbPicture(String thumbPicture) {
+        this.thumbPicture = thumbPicture;
+    }
+
+    public String getMediumPicture() {
+        return mediumPicture;
+    }
+
+    public void setMediumPicture(String mediumPicture) {
+        this.mediumPicture = mediumPicture;
+    }
+
+    public String getLargePicture() {
+        return largePicture;
     }
 
     public void setLargePicture(String largePicture) {
         this.largePicture = largePicture;
     }
 
+    public String getBannerPicture() {
+        return bannerPicture;
+    }
+
+    public void setBannerPicture(String bannerPicture) {
+        this.bannerPicture = bannerPicture;
+    }
+
+    public int getAmountOfComments() {
+        return amountOfComments;
+    }
+
     public void setAmountOfComments(int amountOfComments) {
         this.amountOfComments = amountOfComments;
     }
 
+    public List<String> getTags() {
+        return tags;
+    }
+
     public void setTags(List<String> tags) {
         this.tags = tags;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
     }
 
     public void setComments(List<CommentDTO> comments) {
