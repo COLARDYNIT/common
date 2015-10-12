c974be2d1a44db22ee27eadee527f5be680139e0
SIVANNARAYANAGUNJI <siva.g@mingfu.tw>
2015-10-12 20:38:16 +0530

    10-12-2015 formatting

--- src/main/java/org/hwbot/api/webservice/dto/CompetitionLeaderboardStageDTO.java
+++ src/main/java/org/hwbot/api/webservice/dto/CompetitionLeaderboardStageDTO.java
@@ -2,19 +2,17 @@ package org.hwbot.api.webservice.dto;
 
 
 public class CompetitionLeaderboardStageDTO {
-	
+
 //	- points per stage breakdown
-	
-private String stagepoints;
 
-public String getStagepoints() {
-	return stagepoints;
-}
+    private String stagepoints;
 
-public void setStagepoints(String stagepoints) {
-	this.stagepoints = stagepoints;
-}
+    public String getStagepoints() {
+        return stagepoints;
+    }
 
-
+    public void setStagepoints(String stagepoints) {
+        this.stagepoints = stagepoints;
+    }
 
 }
